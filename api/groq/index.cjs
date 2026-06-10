d2a2bf8c2912d4d12a690007486e9a81ad0c37ae
darkblueorbit <darkblueorbit17@outlook.com>
2026-06-11 01:47:43 +0900

    Signed-off-by: darkblueorbit <darkblueorbit17@outlook.com>

--- api/groq/index.cjs
+++ api/groq/index.cjs
@@ -1,6 +1,6 @@
 const DEFAULT_MODEL = 'llama-3.3-70b-versatile';
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -36,4 +36,4 @@ export default async function handler(req, res) {
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
-}
+};
